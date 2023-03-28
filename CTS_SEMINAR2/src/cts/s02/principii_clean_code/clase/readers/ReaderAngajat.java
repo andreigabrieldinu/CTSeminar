53b97e1d154fa3b8e8037f0c78333e50952ecf7a
Andrei-Gabriel Dinu <82403664+andreigabrieldinu@users.noreply.github.com>
2023-03-28 20:15:32 +0300

    seminar2

---
+++ CTS_SEMINAR2/src/cts/s02/principii_clean_code/clase/readers/ReaderAngajat.java
@@ -0,0 +1,37 @@
+package cts.s02.principii_clean_code.clase.readers;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import cts.s02.principii_clean_code.clase.Angajat;
+import cts.s02.principii_clean_code.clase.Aplicant;
+
+public class ReaderAngajat extends ReaderAplicant {
+
+	public ReaderAngajat(String file) {
+		super(file);
+	}
+
+	@Override
+	public List<Aplicant> readAplicanti() throws FileNotFoundException {
+		Scanner input2 = new Scanner(new File(super.getFileName()));
+		input2.useDelimiter(",");
+		List<Aplicant> angajati = new ArrayList<Aplicant>();
+
+		while (input2.hasNext()) {
+			Angajat angajat=new Angajat();
+			super.citesteAplicant(input2, angajat);
+			angajat.setSalariu(input2.nextInt()); 
+			angajat.setOcupatie(input2.next());
+			angajati.add(angajat);
+			
+		}
+		input2.close();
+		return angajati;	}
+	
+	
+}
